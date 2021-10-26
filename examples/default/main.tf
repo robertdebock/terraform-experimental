3fe1c60244d711bb9bb0d7bc5382bdfeaace8336
Robert de Bock <robert@meinit.nl>
2021-10-26 12:43:15 +0200

    Lets go.

---
+++ examples/default/main.tf
@@ -0,0 +1,3 @@
+module "github_repository" {
+  source = "../../"
+}
---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "github_repository" "default" {
+  name        = "my-example"
+  description = "My awesome example"
+  visibility  = "public"
+}
